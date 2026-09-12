63bb319d47087692301e4f4c69c4ebdfca3a655d
Louis-Olivier Major <louis.olivier.major@gmail.com>
2026-09-12 15:36:39 -0400

    Optimized solution for #1 Two Sum

--- src/easy/p0001_two_sum.rs
+++ src/easy/p0001_two_sum.rs
@@ -1,20 +1,33 @@
+use std::collections::HashMap;
+
 pub struct Solution;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut result: Vec<i32> = Vec::new();
-        for (index_inner_loop, number1) in nums.iter().enumerate() {
-            for (index_outer_loop, number2) in nums.iter().enumerate() {
-                if number1 + number2 == target && index_inner_loop != index_outer_loop {
-                    let index_inner = index_inner_loop as i32;
-                    let index_outer = index_outer_loop as i32;
-                    result.push(index_inner);
-                    result.push(index_outer);
-                    return result;
-                }
+        // SOLUTION #1
+        // This is my first solution. Not optimal
+        // I`using 2 for loop. I can do better
+
+        // for (index1, number1) in nums.iter().enumerate() {
+        //     for (index2, number2) in nums.iter().enumerate() {
+        //         if number1 + number2 == target && index1 != index2 {
+        //             return vec![index1 as i32, index2 as i32];
+        //         }
+        //     }
+        // }
+        // Vec::new()
+
+        // SOLUTION #2
+
+        let mut hash_map_num_index: HashMap<i32, usize> = HashMap::new();
+        for (index, number) in nums.iter().enumerate() {
+            if hash_map_num_index.contains_key(&(target - *number)) {
+                return vec![hash_map_num_index[&(target - *number)] as i32, index as i32];
             }
+
+            hash_map_num_index.insert(*number, index);
         }
-        result
+        Vec::new()
     }
 }
 
