ac4ddc93f088de8308d77af5f6913d0e341f2efc
Louis-Olivier Major <louis.olivier.major@gmail.com>
2026-09-18 20:55:52 -0400

    First and ugly ulgy solution to Longest Common Prefixe

--- src/easy/p0014_longest_common_prefix.rs
+++ src/easy/p0014_longest_common_prefix.rs
@@ -2,7 +2,40 @@ pub struct Solution;
 
 impl Solution {
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
-        String::from("hello world")
+        if strs.len() == 0 {
+            "".to_string();
+        }
+
+        let mut longest_common_prefix = String::from("");
+        let mut shortest_len = 0;
+
+        for (index, word) in strs.iter().enumerate() {
+            if index == 0 {
+                shortest_len = word.len();
+            }
+
+            shortest_len = shortest_len.min(word.len());
+        }
+
+        for i in 0..shortest_len {
+            let mut char_to_add: Option<char> = None;
+            for (index, word) in strs.iter().enumerate() {
+                if index == 0 {
+                    char_to_add = word.chars().nth(i);
+                } else {
+                    if char_to_add != word.chars().nth(i) {
+                        return longest_common_prefix;
+                    }
+                }
+                if index == strs.len() - 1 {
+                    if let Some(c) = char_to_add {
+                        longest_common_prefix.push(c);
+                    }
+                }
+            }
+        }
+
+        longest_common_prefix
     }
 }
 
@@ -24,5 +57,13 @@ mod tests {
         let output = Solution::longest_common_prefix(input);
 
         assert_eq!(output, "");
+    }
+
+    #[test]
+    fn p00014_3() {
+        let input = ["vandal", "vanilla", "van"].map(String::from).to_vec();
+        let output = Solution::longest_common_prefix(input);
+
+        assert_eq!(output, "vandal");
     }
 }
